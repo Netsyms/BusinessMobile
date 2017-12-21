e23ad3b0d57c4941dd41a23490cd9d5b103c20ca
Skylar Ittner <admin@netsyms.com>
2017-12-21 00:34:05 -0700

    Minor code formatting adjustments

--- www/js/app.js
+++ www/js/app.js
@@ -4,28 +4,30 @@
 
 userinfo = null;
 
-document.addEventListener("deviceready", function () {
-    if (cordova.platformId == 'android') {
-	StatusBar.backgroundColorByHexString("#1976d2");
-    }
-
-    // Enable/disable jQuery animations depending on user preference
-    $.fx.off = !(localStorage.getItem("animations") === null || localStorage.getItem("animations") === "true");
-
-    /* Fade out alerts */
-    $(".alert .close").click(function (e) {
-	$(this).parent().fadeOut("slow");
-    });
-
-    if (localStorage.getItem("setupcomplete")) {
-	getuserinfo(function () {
-	    openscreen("home");
-	});
+/**
+ * Switches the app to the given screen.
+ * @param {String} screenname The name of the screen to show.
+ * @param {String} effect FADE, SLIDE, or nothing
+ * @returns {undefined}
+ */
+function openscreen(screenname, effect) {
+    if (effect === 'FADE') {
+        $('#content-zone').fadeOut(300, function () {
+            $('#content-zone').load("views/" + screenname + ".html", function () {
+                $('#content-zone').fadeIn(300);
+            });
+        });
+    } else if (effect === 'SLIDE') {
+        $('#content-zone').slideToggle('400', function () {
+            $('#content-zone').load("views/" + screenname + ".html", function () {
+                $('#content-zone').slideToggle('400');
+            });
+        });
     } else {
-	openscreen("setup1");
+        $('#content-zone').load("views/" + screenname + ".html");
     }
-    setTimeout(navigator.splashscreen.hide, 1000);
-}, false);
+    currentscreen = screenname;
+}
 
 /**
  * Fetch user info (name, email, etc) from the server and save to the global 
@@ -36,67 +38,42 @@ document.addEventListener("deviceready", function () {
 function getuserinfo(callback) {
     $(".loading-text").text("Loading account...");
     $.post(localStorage.getItem("syncurl"), {
-	username: localStorage.getItem("username"),
-	key: localStorage.getItem("key"),
-	password: localStorage.getItem("password"),
-	action: "user_info"
+        username: localStorage.getItem("username"),
+        key: localStorage.getItem("key"),
+        password: localStorage.getItem("password"),
+        action: "user_info"
     }, function (data) {
-	if (data.status === 'OK') {
-	    $(".loading-text").text("Loading...");
-	    userinfo = data.info;
-	    if (typeof callback == 'function') {
-		callback();
-	    }
-	} else {
-	    navigator.notification.alert(data.msg, null, "Error", 'Dismiss');
-	    openscreen("homeloaderror");
-	}
+        if (data.status === 'OK') {
+            $(".loading-text").text("Loading...");
+            userinfo = data.info;
+            if (typeof callback == 'function') {
+                callback();
+            }
+        } else {
+            navigator.notification.alert(data.msg, null, "Error", 'Dismiss');
+            openscreen("homeloaderror");
+        }
     }, "json").fail(function () {
-	navigator.notification.alert("Could not connect to the server.  Try again later.", null, "Error", 'Dismiss');
-	openscreen("homeloaderror");
+        navigator.notification.alert("Could not connect to the server.  Try again later.", null, "Error", 'Dismiss');
+        openscreen("homeloaderror");
     });
-}
-
-/**
- * Switches the app to the given screen.
- * @param {String} screenname The name of the screen to show.
- * @param {String} effect FADE, SLIDE, or nothing
- * @returns {undefined}
- */
-function openscreen(screenname, effect) {
-    if (effect === 'FADE') {
-	$('#content-zone').fadeOut(300, function () {
-	    $('#content-zone').load("views/" + screenname + ".html", function () {
-		$('#content-zone').fadeIn(300);
-	    });
-	});
-    } else if (effect === 'SLIDE') {
-	$('#content-zone').slideToggle('400', function () {
-	    $('#content-zone').load("views/" + screenname + ".html", function () {
-		$('#content-zone').slideToggle('400');
-	    });
-	});
-    } else {
-	$('#content-zone').load("views/" + screenname + ".html");
-    }
-    currentscreen = screenname;
 }
 
 function openfragment(fragment, target, effect) {
     if (effect === 'FADE') {
-	$(target).fadeOut('slow', function () {
-	    $(target).load("views/" + fragment + ".html", function () {
-		$(target).fadeIn('slow');
-	    });
-	});
+        $(target).fadeOut('slow', function () {
+            $(target).load("views/" + fragment + ".html", function () {
+                $(target).fadeIn('slow');
+            });
+        });
     } else if (effect === 'SLIDE') {
-	$(target).slideToggle('400', function () {
-	    $(target).load("views/" + fragment + ".html", function () {
-		$(target).slideToggle('400');
-	    });
-	});
+        $(target).slideToggle('400', function () {
+            $(target).load("views/" + fragment + ".html", function () {
+                $(target).slideToggle('400');
+            });
+        });
     } else {
-	$(target).load("views/" + fragment + ".html");
+        $(target).load("views/" + fragment + ".html");
     }
 }
 
@@ -112,45 +89,45 @@ function openfragment(fragment, target, effect) {
 function setnavbar(type, title, returnscreen) {
     var navbar = $('#navbar-header');
     if (type == false) {
-	$('#navbar').css('display', 'none');
-	$('#content-zone').css('margin-top', '0px');
+        $('#navbar').css('display', 'none');
+        $('#content-zone').css('margin-top', '0px');
     } else {
-	if (cordova.platformId == 'android') {
-	    StatusBar.backgroundColorByHexString("#1976d2");
-	    window.plugins.headerColor.tint("#2196f3");
-	} else {
-	    StatusBar.backgroundColorByHexString("#2196f3");
-	}
-	$('#navbar').css('display', 'initial');
-	$('#content-zone').css('margin-top', '75px');
-	if (returnscreen === undefined) {
-	    returnscreen = "home";
-	    _returnscreen = null;
-	} else {
-	    _returnscreen = returnscreen;
-	}
-	navbar.fadeOut(150, function () {
-	    switch (type) {
-		case "home":
-		    navbar.html('<span class="navbar-brand" style="color: white;">Business</span><span class="navbar-brand pull-right"><span onclick="openscreen(\'mobilecode\', \'FADE\')"><img src="icons/ic_lock.svg" alt="" /></span> &nbsp; <span onclick="openscreen(\'otp\', \'FADE\')"><img src="icons/ic_vpn_key.svg" alt="" /></span> &nbsp; <span onclick="openscreen(\'settings\', \'FADE\')"><img src="icons/ic_settings.svg" alt="" /></span></span>');
-		    break;
-		case "setup":
-		    navbar.html('<span class="navbar-brand" style="color: white;">Business</span><span class="navbar-brand pull-right"><span onclick="openscreen(\'otp\', \'FADE\')"><img src="icons/ic_vpn_key.svg" alt="" /></span> &nbsp; <span onclick="openscreen(\'settings\', \'FADE\')"><img src="icons/ic_settings.svg" alt="" /></span></span>');
-		    break;
-		case "settings":
-		    navbar.html('<span class="navbar-brand pull-left" style="color: white;" onclick="openscreen(\'home\', \'FADE\')"><img src="icons/ic_arrow-back.svg" /></span><span class="navbar-brand navbar-title" style="color: white;" onclick="openscreen(\'home\', \'FADE\')">Settings</span>');
-		    break;
-		case "otp":
-		    navbar.html('<span class="navbar-brand pull-left" style="color: white;" onclick="openscreen(\'home\', \'FADE\')"><img src="icons/ic_arrow-back.svg" /></span><span class="navbar-brand navbar-title" style="color: white;" onclick="openscreen(\'home\', \'FADE\')">Auth Keys</span><span class="navbar-brand pull-right" onclick="openscreen(\'addotp\', \'FADE\')"><img src="icons/ic_add.svg" alt="" /></span>');
-		    break;
-		case "app":
-		    navbar.html('<span class="navbar-brand pull-left" style="color: white;" onclick="openscreen(\'' + returnscreen + '\', \'FADE\')"><img src="icons/ic_arrow-back.svg" /></span><span class="navbar-brand navbar-title" style="color: white;" onclick="openscreen(\'' + returnscreen + '\', \'FADE\')">' + title + '</span>');
-		    break;
-		default:
-		    navbar.html('<span class="navbar-brand" style="color: white;">Business</span>');
-	    }
-	    navbar.fadeIn(150);
-	});
+        if (cordova.platformId == 'android') {
+            StatusBar.backgroundColorByHexString("#1976d2");
+            window.plugins.headerColor.tint("#2196f3");
+        } else {
+            StatusBar.backgroundColorByHexString("#2196f3");
+        }
+        $('#navbar').css('display', 'initial');
+        $('#content-zone').css('margin-top', '75px');
+        if (returnscreen === undefined) {
+            returnscreen = "home";
+            _returnscreen = null;
+        } else {
+            _returnscreen = returnscreen;
+        }
+        navbar.fadeOut(150, function () {
+            switch (type) {
+                case "home":
+                    navbar.html('<span class="navbar-brand" style="color: white;">Business</span><span class="navbar-brand pull-right"><span onclick="openscreen(\'mobilecode\', \'FADE\')"><img src="icons/ic_lock.svg" alt="" /></span> &nbsp; <span onclick="openscreen(\'otp\', \'FADE\')"><img src="icons/ic_vpn_key.svg" alt="" /></span> &nbsp; <span onclick="openscreen(\'settings\', \'FADE\')"><img src="icons/ic_settings.svg" alt="" /></span></span>');
+                    break;
+                case "setup":
+                    navbar.html('<span class="navbar-brand" style="color: white;">Business</span><span class="navbar-brand pull-right"><span onclick="openscreen(\'otp\', \'FADE\')"><img src="icons/ic_vpn_key.svg" alt="" /></span> &nbsp; <span onclick="openscreen(\'settings\', \'FADE\')"><img src="icons/ic_settings.svg" alt="" /></span></span>');
+                    break;
+                case "settings":
+                    navbar.html('<span class="navbar-brand pull-left" style="color: white;" onclick="openscreen(\'home\', \'FADE\')"><img src="icons/ic_arrow-back.svg" /></span><span class="navbar-brand navbar-title" style="color: white;" onclick="openscreen(\'home\', \'FADE\')">Settings</span>');
+                    break;
+                case "otp":
+                    navbar.html('<span class="navbar-brand pull-left" style="color: white;" onclick="openscreen(\'home\', \'FADE\')"><img src="icons/ic_arrow-back.svg" /></span><span class="navbar-brand navbar-title" style="color: white;" onclick="openscreen(\'home\', \'FADE\')">Auth Keys</span><span class="navbar-brand pull-right" onclick="openscreen(\'addotp\', \'FADE\')"><img src="icons/ic_add.svg" alt="" /></span>');
+                    break;
+                case "app":
+                    navbar.html('<span class="navbar-brand pull-left" style="color: white;" onclick="openscreen(\'' + returnscreen + '\', \'FADE\')"><img src="icons/ic_arrow-back.svg" /></span><span class="navbar-brand navbar-title" style="color: white;" onclick="openscreen(\'' + returnscreen + '\', \'FADE\')">' + title + '</span>');
+                    break;
+                default:
+                    navbar.html('<span class="navbar-brand" style="color: white;">Business</span>');
+            }
+            navbar.fadeIn(150);
+        });
     }
 }
 
@@ -167,11 +144,11 @@ function setnavbar(type, title, returnscreen) {
  */
 function openapp(id, api, url, icon, title, injectcode, shownavbar) {
     $('#content-zone').fadeOut(300, function () {
-	$('#content-zone').load("views/app.html", function () {
-	    $('#content-zone').fadeIn(300, function () {
-		launchapp(id, api, url, icon, title, injectcode, shownavbar);
-	    });
-	});
+        $('#content-zone').load("views/app.html", function () {
+            $('#content-zone').fadeIn(300, function () {
+                launchapp(id, api, url, icon, title, injectcode, shownavbar);
+            });
+        });
     });
 }
 
@@ -183,8 +160,8 @@ function openapp(id, api, url, icon, title, injectcode, shownavbar) {
  */
 function openmodal(filename, modalselector) {
     $('#modal-load-box').load("views/" + filename + ".html", null, function (x) {
-	$(modalselector).css('z-index', 9999999);
-	$(modalselector).modal('show');
+        $(modalselector).css('z-index', 9999999);
+        $(modalselector).modal('show');
     });
 }
 
@@ -201,28 +178,51 @@ function restartApplication() {
     navigator.splashscreen.show();
     // We're doing the timeout so we don't run afoul of server-side rate limiting
     setTimeout(function () {
-	window.location = "index.html";
+        window.location = "index.html";
     }, 3000);
 }
 
 // Handle back button to close things
 document.addEventListener("backbutton", function (event) {
     if (localStorage.getItem("setupcomplete")) {
-	if ($("#appframe").length && historyctr > 0) {
-	    console.log("going back");
-	    var iframe = document.getElementById("appframe");
-	    iframe.contentWindow.postMessage("goback", "*");
-	    historyctr--;
-	} else if (_returnscreen != null) {
-	    openscreen(_returnscreen, "FADE");
-	    _returnscreen = null;
-	} else {
-	    openscreen("home", "FADE");
-	}
+        if ($("#appframe").length && historyctr > 0) {
+            console.log("going back");
+            var iframe = document.getElementById("appframe");
+            iframe.contentWindow.postMessage("goback", "*");
+            historyctr--;
+        } else if (_returnscreen != null) {
+            openscreen(_returnscreen, "FADE");
+            _returnscreen = null;
+        } else {
+            openscreen("home", "FADE");
+        }
     } else {
-	if (_returnscreen != null) {
-	    openscreen(_returnscreen, "FADE");
-	    _returnscreen = null;
-	}
+        if (_returnscreen != null) {
+            openscreen(_returnscreen, "FADE");
+            _returnscreen = null;
+        }
     }
+}, false);
+
+document.addEventListener("deviceready", function () {
+    if (cordova.platformId == 'android') {
+        StatusBar.backgroundColorByHexString("#1976d2");
+    }
+
+    // Enable/disable jQuery animations depending on user preference
+    $.fx.off = !(localStorage.getItem("animations") === null || localStorage.getItem("animations") === "true");
+
+    /* Fade out alerts */
+    $(".alert .close").click(function (e) {
+        $(this).parent().fadeOut("slow");
+    });
+
+    if (localStorage.getItem("setupcomplete")) {
+        getuserinfo(function () {
+            openscreen("home");
+        });
+    } else {
+        openscreen("setup1");
+    }
+    setTimeout(navigator.splashscreen.hide, 1000);
 }, false);
--- www/js/sidemenu.js
+++ www/js/sidemenu.js
@@ -9,12 +9,12 @@
 // This code from
 // https://stackoverflow.com/a/6177502
 $.cssHooks.backgroundColor = {
-    get: function(elem) {
+    get: function (elem) {
         if (elem.currentStyle)
             var bg = elem.currentStyle["backgroundColor"];
         else if (window.getComputedStyle)
             var bg = document.defaultView.getComputedStyle(elem,
-                null).getPropertyValue("background-color");
+                    null).getPropertyValue("background-color");
         if (bg.search("rgb") == -1)
             return bg;
         else {
@@ -60,11 +60,11 @@ $(document).ready(function () {
     var logo = "__LOGO__";
     $('body').append("<div id='swipe-nav'><div id='swipe-header' style='background-color: " + menucolor + "; color: " + textcolor + "'><a href='./app.php'><img id='swipe-appicon' src='" + logo + "' /></a> <div id='swipe-username'><i class='fa fa-user fa-fw'></i> " + username + "</div></div>\n<ul id='swipe-pages'>" + pages + "</ul><ul><li><a onclick='quitapp()'><i class='fa fa-sign-out fa-fw'></i> Back to Menu</a></li></ul></div>");
     $('body').append("<div id='swipe-shader'></div>");
-    
+
     $(".navbar-brand").attr("href", "#");
-    
+
     parent.postMessage("setcolor " + menucolor, "*");
-    
+
     $('button.navbar-toggle[data-toggle="collapse"]').click(togglemenu);
 
     $('#swipe-shader').click(togglemenu);
@@ -77,11 +77,11 @@ $(document).ready(function () {
             openmenu();
         }
     });
-    
+
     Hammer(document.body).on("swipeleft", function (e) {
         closemenu();
     });
-    
+
     window.addEventListener('message', function (event) {
         if (event.data.startsWith("coderesult~|~")) {
             var data = event.data.split("~|~");
@@ -92,12 +92,12 @@ $(document).ready(function () {
             $(ref).trigger("change");
             console.log("app: received " + event.data);
         } else if (event.data.startsWith("goback")) {
-	    console.log("app: received " + event.data);
-	    window.history.back();
-	    parent.postMessage('goneback','*');
-	}
+            console.log("app: received " + event.data);
+            window.history.back();
+            parent.postMessage('goneback', '*');
+        }
     });
-    
+
     setInterval(function () {
         $.getJSON("mobile/index.php", {action: "ping"}, function (d) {
             console.log("app: keepalive ping " + d.status);
@@ -106,7 +106,7 @@ $(document).ready(function () {
 });
 
 function quitapp() {
-    parent.postMessage('quit','*');
+    parent.postMessage('quit', '*');
 }
 
 function scancode(refstring) {
